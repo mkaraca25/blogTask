f9fe6978751efb3aa73ee2f361f4e14c7c867181
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2023-03-31 19:55:50 +0300

    Responsive mobil navbar and post shard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
